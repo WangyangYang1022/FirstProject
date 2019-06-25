6127949c63ac7454995fe4b03e7ab1c02ce0e03b
wyy <1271496547@qq.com>
2019-06-25 16:41:00 +0800

    test

--- app/src/main/java/com/example/firstproject/MainActivity.java
+++ app/src/main/java/com/example/firstproject/MainActivity.java
@@ -3,12 +3,19 @@ package com.example.firstproject;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import com.amap.api.maps2d.AMap;
+import com.amap.api.maps2d.MapView;
+
+public class MainActivity extends BaseActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        requestPermission();
         //测试
+        MapView mapView = (MapView) findViewById(R.id.map);
+        mapView.onCreate(savedInstanceState);// 此方法必须重写
+        AMap aMap = mapView.getMap();
     }
 }
